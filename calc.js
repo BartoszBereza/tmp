8d2e24f3da25f785cf7b35defd1fef9f6f7b2230
BartoszBereza <bbplen@gmail.com>
2020-10-20 16:34:38 +0200

    Delete calc.js

--- calc.js
+++
@@ -1,9 +0,0 @@
-function calc(event){
-	var firstElement = document.getElementById("first");
-	var secondElement = document.getElementById("second");
-	var resultElement = document.getElementById("result");
-	
-	result = Number(firstElement.value) + Number(secondElement.value);
-	resultElement.innerHTML = result;
-}
-
